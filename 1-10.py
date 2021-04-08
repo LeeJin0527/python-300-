a1104426b52514d97ea2186ba0222fee1baa3c86
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-04-08 23:18:59 +0900

    Add files via upload
    파이썬 시작하기

---
+++ 1-10.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 23:17:50 2021
+
+@author: ijin
+"""
+
+#001~010
+print("Hello World")
+print('Hello World')
+
+#%%
+print("Mary's cosmetics")
+
+#%%
+print('신씨가 소리 질렀다. "도둑이야".')
+
+#%%
+print("C:\Windows")
+#%%
+print("안녕하세요.\n 만나서\t\t반갑습니다.")
+
+#%%
+#쉼표는 공백
+print("오늘은","일요일")
+
+#%%
+print("naver","kakao","sk","samsung",sep=";")
+
+#%%
+print("naver","kakao","sk","samsung",sep="/")
+
+#%%
+print("first ",end="");print("second")
+
+#%%
+print(5/3)
