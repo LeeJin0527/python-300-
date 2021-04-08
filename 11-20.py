c5323244791ae0438985f1c8a791072dcf08ba91
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-04-08 23:41:43 +0900

    Add files via upload

---
+++ 11-20.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  8 23:19:15 2021
+
+@author: ijin
+"""
+
+#11-20
+Samsung = 50000
+Joo = 10
+Total = Samsung * Joo
+
+print(Total)
+#%%
+
+삼성전자 = 50000
+총평가금액 = 삼성전자 * 10
+print(총평가금액)
+
+#%%
+시가총액 = 2980000000000000
+현재가 = 50000
+PER = 15.79
+
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+#%%
+s = "hello"
+t = "python"
+
+print(s + "!",t)
+
+#%%
+print( 2 + 2 * 3)
+
+#%%
+a = 128
+print(type(a))
+a ="132"
+print(type(a))
+
+#%%
+num_str ="720"
+num_int = int(num_str)
+print(num_int + 1, type(num_int))
+
+#%%
+num = 100
+num_str =str(num)
+print(num_str)
+
+#%%
+num_str = "15.79"
+num_flo = float(num_str)
+print(num_flo ,type(num_flo))
+
+#%%
+year = "2020"
+year_int = int(year)
+for i in range(3):
+    print(year_int + i +1)
+    
+#%%
+month = 48584
+interest = 36
+
+total = month * interest
+print(total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
