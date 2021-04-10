787d6d5e4ca8264cd8e30e28aed668d92fb985db
JIN <64263694+LeeJin0527@users.noreply.github.com>
2021-04-10 19:46:05 +0900

    파이썬 분기문

---
+++ 101-110.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 10 19:39:23 2021
+
+@author: jin
+"""
+
+#%%
+#101-110
+
+#%% 
+num =1
+print(bool(num))
+
+#%%
+print(3 ==5)
+
+#%%
+print(3 < 5)
+
+#%%
+x = 4
+print(1 < x < 5)
+
+#%%
+print((3 == 3) and ( 4 != 3))
+
+#%%
+print( 3>= 4)
+
+#%%
+if 4 < 3:
+    print("Hello World")
+    
+    
+#%%
+if 4 < 3:
+    print("Hello World")
+else:
+    print("Hi, There")
+    
+#%%
+if True:
+    print("1")
+    print("2")
+else:
+    print("3")
+print("4")
+
+#%%
+if True:
+    if False:
+        print("1")
+        print("2")
+        
+    else:
+        print("3")
+else:
+    print("4")
+print("5")
